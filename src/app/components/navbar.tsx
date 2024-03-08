d0172e5e07578612b0039b0ed92a0bea1d5ec35f
Waris <warisareshi@gmail.com>
2024-03-08 07:51:16 +0530

    Updated Navbar Links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#mission"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Mission
@@ -71,7 +71,7 @@ export default function Navbar() {
             </li>
             <li>
               <a
-                href="https://google.com/"
+                href="https://wa.me/919469759031?text=Hello%20Ascendifyr!"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
