782fcf7be0e4bd2cf45c2cc9c89a7de8cfdfff08
Waris <warisareshi@gmail.com>
2024-03-08 09:23:42 +0530

    Add Closing and change structure to nav-hero-features-pricing-closing-about-footer

---
+++ src/app/components/closing.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function Closing() {
+  return (
+    <div id="closing">
+      <div className="flex flex-col items-center justify-center mt-8">
+        <h1 className="text-3xl font-bold">
+          Click the Button below to Join our Community and Stay Updated!
+        </h1>
+        <h2 className="text-gray-500">
+          Be the first to get access to the software and get insider updates.
+        </h2>
+        <h2 className="text-gray-500">
+          Connect with like-minded-individuals (fellow Agency Owners,
+          Entrepreneurs etc)
+        </h2>
+        <button className="flex flex-row bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded text-2xl mt-3">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+            width={32}
+            height={32}
+            alt="Whatsapp Logo"
+          />
+          <a href="https://chat.whatsapp.com/DXbh1xx7ECAFQwSu4TUbWo">
+            JOIN TODAY!!!
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,18 @@ import Pricing from "@/app/components/pricing";
 import Footer from "./components/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import About from "@/app/components/about";
+import Closing from "@/app/components/closing";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
       <Hero></Hero>
-      <About></About>
       <Features></Features>
       <Pricing></Pricing>
+      <Closing></Closing>
+      <About></About>
+      <Footer></Footer>
     </main>
   );
 }
