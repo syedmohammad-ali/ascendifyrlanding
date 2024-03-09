8256731ab97908237d5ec5645ce9bd008d0ae61b
Waris <warisareshi@gmail.com>
2024-03-09 13:23:54 +0530

    Changing a line in the hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,7 +5,7 @@ export default function hero() {
     <div className="flex flex-col h-screen bg-[url('/hero_background.png')] bg-no-repeat bg-cover h-screen items-center justify-center">
       <h1 className="text-6xl font-bold text-white">Welcome to Ascendifyr!</h1>
       <h3 className="text-1xl mt-2 text-white mb-10">
-        We are the revolution that Agency Owners have been waiting for!
+        We are the revolution that Agency Owners have been wanting for ages!
       </h3>
       <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-2xl">
         Get Started!
