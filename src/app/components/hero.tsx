604a1a2e96440f5fad3a291f7ec642a7645c1d2c
Waris <warisareshi@gmail.com>
2024-03-09 17:29:44 +0530

    Changing hero Buttons

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -7,9 +7,25 @@ export default function hero() {
       <h3 className="text-1xl mt-2 text-white mb-10">
         We are the revolution that Agency Owners have been wanting for ages!
       </h3>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded text-2xl">
-        Get Started!
+      <button className="flex flex-row bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded text-2xl mt-3">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+          width={32}
+          height={32}
+          alt="Whatsapp Logo"
+        />
+        <a href="https://chat.whatsapp.com/DXbh1xx7ECAFQwSu4TUbWo">
+          Join the Whatsapp Community
+        </a>
       </button>
+
+      <button className="flex flex-row bg-white hover:bg-blue-200 text-black font-bold py-2 px-4 rounded text-2xl mt-3">
+        <Image src="/linkedin.png" width={29} height={29} alt="Linkedin Logo" />
+        <a href="https://chat.whatsapp.com/DXbh1xx7ECAFQwSu4TUbWo">
+          &nbsp;Follow us on LinkedIn
+        </a>
+      </button>
+      
     </div>
   );
 }
